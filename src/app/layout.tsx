93754356b13bb2480455a2a4611376b49bdde064
Giuliano Crivelli <giuliano.crivelli@byside.com>
2024-04-18 11:58:42 -0400

    adding src with pages and app folder with tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head />
       <body>{children}</body>
     </html>
   );
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <span className="px-10">test color</span>;
+}
